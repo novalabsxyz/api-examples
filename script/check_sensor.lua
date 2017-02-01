242a35923e6333866227e14dc9828168ad1ead9d
Marc Nijdam <marc@helium.com>
2017-02-01 14:10:37 -0800

    Add datasheet link to scripts as header comments

--- script/check_sensor.lua
+++ script/check_sensor.lua
@@ -1,3 +1,6 @@
+-- Check availability of the on-baord sensor
+-- Datasheet: http://www.st.com/content/ccc/resource/technical/document/datasheet/bf/c1/4f/23/61/17/44/8a/DM00140895.pdf/files/DM00140895.pdf/jcr:content/translations/en.DM00140895.pdf
+
 i2c = he.i2c
 
 LPS22HBTR_ADDR = 0x5C
