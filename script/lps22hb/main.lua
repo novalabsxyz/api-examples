76e8f321fceff80ecdf0c74ef278e98f4a42f5ee
Marc Nijdam <marc@helium.com>
2017-03-03 16:21:26 -0800

    Switch to library approach for built in sensor.
    Use helium-script 2.x to package and run this by:
    
    semi-hosted: `helium-scipt -m main.lua lps22hb.lua`
    upload: `helium-scipt -u -m main.lua lps22hb.lua`
    
    interactive use of the library:
    
    `helium-scipt -i lps22hb.lua`
    
    then you can do:
    
    ```
    > lps22hb = require("lps22hb")
    > sensor = lps22hb:new()
    > sensor:read_temperature()
    ```

---
+++ script/lps22hb/main.lua
@@ -0,0 +1,24 @@
+lps22hb = require('lps22hb')
+
+SAMPLE_INTERVAL = 60000 -- 1 minute
+
+-- construct the sensor
+sensor = assert(lps22hb:new())
+-- get current time
+local now = he.now()
+while true do
+    -- take readings
+    local pressure = assert(sensor:read_pressure())
+    local temperature = assert(sensor:read_temperature())
+
+    -- send temperature as a float "f" on port "t"
+    he.send("t", now, "f", temperature)
+    -- send pressure as a float "f" on port "p"
+    he.send("p", now, "f", pressure)
+
+    -- Un-comment the following line to see results in semi-hosted mode
+    -- print(temperature, pressure)
+
+    -- wait for SAMPLE_INTERVAL time
+    now = he.wait{time=now + SAMPLE_INTERVAL}
+end
